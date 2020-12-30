a6a7890b308271a17d9897e68d7d3c42e9efe1ae
Anusha <adhamera@gmail.com>
2020-12-29 21:18:22 -0600

    updated final

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,6 @@
 //Global variables
-let urlgeocoding;
 var marker;
-let userLat, userLon;
+
 
 $("form").on("submit", function(e){
     //Grabing user input of destination
@@ -9,16 +8,13 @@ $("form").on("submit", function(e){
     const city = $("#city-text").val();
     const state = $("#state-text").val();
     console.log(`${city} and ${state}`);
-
-    //Show weatherat destination
+=
     $(document).ready(function(){
         $.ajax({
             url: `https://api.openweathermap.org/data/2.5/weather?q=${city},${state}&units=imperial&appid=849afef2e7c9aca1f133d01fb20ebbd5`,
             method: "GET",
         }).then(function(response) {
-           /*  userLat = response.results[0].geometry.location.lat;
-            userLon = response.results[0].geometry.location.lng;
-            getWeatherByLatLng(userLat, userLon); */
+           
             $(".temp").text("Current Weather: " + response.weather[0].description);
             $(".desc").text("Temperature: " + Math.floor(response.main.temp));
             $(".wind").text(
